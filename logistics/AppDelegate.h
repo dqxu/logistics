42d9e57629a5e1f4bccdfffc668bb246e69b70a4
ddqq <wgyptrade@163.com>
2016-03-03 14:11:22 +0800

    Initial Commit

---
+++ logistics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  logistics
+//
+//  Created by wgyp on 16/3/3.
+//  Copyright © 2016年 dqxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
